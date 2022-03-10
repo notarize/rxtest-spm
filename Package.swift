16b38bd08d2e81d0c509beb18b365c103f7c68c3
Omar Inam <omar@notarize.com>
2022-03-10 10:47:12 -0800

    updated to RxTest/Blocking v6.5

--- Package.swift
+++ Package.swift
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "RxTest",
-                      url: "https://ios-notarize-signer-sdk.s3.us-west-1.amazonaws.com/RxTest-X13-6.2.0.xcframework.zip",
-                      checksum: "048d9930315ed840e3831e95ed0ecbfe85de3fb9c2da82aa79b49ff6bb32da3e"),
+                      url: "https://ios-notarize-signer-sdk.s3.us-west-1.amazonaws.com/RxTest-X13.1-6.5.0.xcframework.zip",
+                      checksum: "41b2cc0865afae57b1832cebf164b66ce88907669bdd75cd9dddbb36ad14fee2"),
         .binaryTarget(name: "RxBlocking",
-                      url: "https://ios-notarize-signer-sdk.s3.us-west-1.amazonaws.com/RxBlocking-X13-6.2.0.xcframework.zip",
-                      checksum: "cb2c19161ffc7d00e2ab9ef38747df53a2ce250ba4f99febbd2bd852ad95677e")
+                      url: "https://ios-notarize-signer-sdk.s3.us-west-1.amazonaws.com/RxBlocking-X13.1-6.5.0.xcframework.zip",
+                      checksum: "f1b4f0dfe05afe300e2c6e24601c317e5065f902d1f5affeb6020287f74f38f8")
     ]
 )
